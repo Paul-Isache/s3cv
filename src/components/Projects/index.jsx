ff87d5dd1540ae619653f7493eac1c2eeb10b391
Paul Isache <paulisache@Pauls-MacBook-Pro.local>
2019-06-19 23:25:54 +0300

    Add projects component

---
+++ src/components/Projects/index.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react'
+import moment from 'moment'
+
+import { UserContext } from '../../contexts/UserContext';
+
+import './Projects.css'
+
+function Projects() {
+  const { user } = useContext(UserContext)
+  let projects
+
+  if (user && user.projects) {
+    projects = user.projects.map((item, index) => Project(item, index))
+  }
+
+  return (
+    <div className="projects">
+      <h4><code>Projects</code></h4>
+      {projects}
+    </div>
+  )
+}
+
+function Project({ title, company, description, date }, index) {
+  return (
+    <div className="project-wrapper" key={`project-${index}`}>
+      <h5><span role='img'>🚀</span><code>{title}</code> at <strong>{company}</strong></h5>
+      <span>
+        {!date.end ? 'now' : moment.unix(date.end).format('MMM YYYY')} - {moment.unix(date.start).format('MMM YYYY')}
+      </span>
+      <p>
+        {description}
+      </p>
+    </div>
+  )
+
+}
+
+export default Projects
